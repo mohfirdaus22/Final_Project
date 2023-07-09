3f0c9b75fce29f6457b55416b11bcde4f36f7c57
mohfirdaus22 <thepashter6@gmail.com>
2023-07-09 22:26:00 +0700

    Add layout table, label,  textbox

--- Final_Project/Form2.cs
+++ Final_Project/Form2.cs
@@ -16,5 +16,10 @@ namespace Final_Project
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
