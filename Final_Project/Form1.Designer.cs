8ce741965becb0e1b76310141e6f5ebd4e45fe7a
mohfirdaus22 <thepashter6@gmail.com>
2023-07-10 17:47:05 +0700

    Add method klik button Menu,koki,suplier

--- Final_Project/Form1.Designer.cs
+++ Final_Project/Form1.Designer.cs
@@ -63,6 +63,7 @@
             this.button3.TabIndex = 2;
             this.button3.Text = "Menu";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
@@ -72,6 +73,7 @@
             this.button4.TabIndex = 3;
             this.button4.Text = "Koki";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
@@ -81,6 +83,7 @@
             this.button5.TabIndex = 4;
             this.button5.Text = "Suplier";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // Form1
             // 
--- Final_Project/Form1.cs
+++ Final_Project/Form1.cs
@@ -30,5 +30,26 @@ namespace Final_Project
             f3.Show();
             this.Hide();
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form4 f4 = new Form4();
+            f4.Show();
+            this.Hide();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Form5 f5 = new Form5();
+            f5.Show();
+            this.Hide();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Form6 f6 = new Form6();
+            f6.Show();
+            this.Hide();
+        }
     }
 }
