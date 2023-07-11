836112d794feec563b8d2eb35c419b8440c70a7d
M S I <M S I@MSI>
2023-07-11 13:04:35 +0700

    add button

--- Final_Project/Form6.Designer.cs
+++ Final_Project/Form6.Designer.cs
@@ -33,6 +33,7 @@
             this.btnClear = new System.Windows.Forms.Button();
             this.btnBack = new System.Windows.Forms.Button();
             this.btnOpen = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnAdd
@@ -80,11 +81,22 @@
             this.btnOpen.Text = "Open";
             this.btnOpen.UseVisualStyleBackColor = true;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(246, 105);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 50);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form6
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnOpen);
             this.Controls.Add(this.btnBack);
             this.Controls.Add(this.btnClear);
@@ -103,5 +115,6 @@
         private System.Windows.Forms.Button btnClear;
         private System.Windows.Forms.Button btnBack;
         private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button button1;
     }
 }
